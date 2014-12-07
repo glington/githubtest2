247fdb79145d0deb59e5f909def692e78ba2f15c
Jun Oura <jun@Jun-no-MacBook.local>
2014-12-07 21:46:23 +0900

    Initial Commit

---
+++ githubtest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubtest2
+//
+//  Created by Jun Oura on 2014/12/07.
+//  Copyright (c) 2014年 systems-power. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubtest2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubtest2
+//
+//  Created by Jun Oura on 2014/12/07.
+//  Copyright (c) 2014年 systems-power. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
